ad08c42a84134e311820809ca10eb4819f7cd897
Operel <oriperelman@gmail.com>
2019-05-29 16:43:51 +0300

    add layout to page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Layout from "../components/layout";
-import SEO from '../components/seo';
+import Sidebar from '../components/sidebar';
 // import Img from "gatsby-image";
 
 export default ({ pageContext }) => {
   return (
     <Layout>
-      <SEO title={pageContext.title} />
       {/*<Img fluid={pageContext.featured_media.localFile.childImageSharp.fluid}/>*/}
-      <h1>{pageContext.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: pageContext.content }}></div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-sm-12 col-xl-9" style={{borderLeft: '0.5px solid'}}>
+            <h1>{pageContext.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: pageContext.content }}></div>
+          </div>
+          <div className="col col-sm-0 col-xl-3">
+            <Sidebar />
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
