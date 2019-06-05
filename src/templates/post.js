3c41a54432531fcc270028fefd9028e79a56a680
Operel <oriperelman@gmail.com>
2019-06-06 00:24:36 +0300

    add post featured image

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Layout from "../components/layout";
 import PageLayout from '../components/pageLayout';
+import Img from 'gatsby-image';
 
-export default ({ pageContext }) => {
+export default ({ pageContext, data }) => {
   return (
     <Layout>
       <PageLayout>
         <h1>{pageContext.title}</h1>
+        <Img
+        fluid={data.wordpressPost.featured_media.localFile.childImageSharp.fluid}
+        style={{ margin: '20px 0' }}
+        />
         <p dangerouslySetInnerHTML={{ __html: pageContext.content }}></p>
       </PageLayout>
     </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    wordpressPost(id: { eq: $id }) {
+      id
+      slug
+      featured_media {
+        localFile {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }`
