30218b5e1e1a022bb564c6ea77c8ab33c5a9ec00
Operel <oriperelman@gmail.com>
2019-06-08 17:05:39 +0300

    add services section and service components

---
+++ src/components/indexComponents/serviceExcerpt.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import './serviceExcerpt.css';
+
+const ServiceExcerpt = ({ page }) => {
+  const { fixed } = page.acf.icon.localFile.childImageSharp;
+  return (
+    <div className="service">
+      <div>
+        <h4 dangerouslySetInnerHTML={{ __html: page.title }}></h4>
+        <p dangerouslySetInnerHTML={{ __html: page.excerpt }}></p>
+      </div>
+      <Img fixed={fixed}/>
+    </div>
+  )
+}
+
+export default ServiceExcerpt;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Hero from '../components/indexComponents/hero';
+import Services from '../components/indexComponents/services';
 
 export default () => {
   return (
     <Layout>
       <Hero />
+      <Services />
       <div style={{
         backgroundColor: '#bfbfbf',
         height: '70vh'
