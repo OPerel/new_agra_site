683fad574c5b853db2089e3a207304ec5d2e8329
Operel <oriperelman@gmail.com>
2019-06-17 10:33:19 +0300

    install react-spring and slideUp to hero section

--- src/components/indexComponents/hero.js
+++ src/components/indexComponents/hero.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import BgImg from 'gatsby-background-image';
+import { animated, useTrail, config } from 'react-spring';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Button from '../button';
 import './hero.css';
 
 const Hero = () => {
+  const trail = useTrail(1, {
+    config: config.slow,
+    transform: 'translateY(0)',
+    from: { transform: 'translateY(400%)' }
+  })
   return (
     <StaticQuery
       query={graphql`
@@ -22,22 +28,24 @@ const Hero = () => {
         }`
       } render={data => {
         const img = data.wordpressWpMedia.localFile.childImageSharp.fluid;
-        return (
+        return trail.map(animateProps => (
           <>
             <BgImg className="hero" fluid={img}>
               <div className="hero-content">
-                <h1>אגרא יעוץ ותכנון בע"מ</h1>
-                <h4>ניהול ותכנון – מתארי, פרוגרמטי, פונקציונלי, הנדסי וסביבתי. עשרות שנות ניסיון ביעוץ ותכנון לתעשייה, לוגיסטיקה, משרדי ממשלה, רשויות ממלכתיות ומקומיות, פרויקטים סביבתיים.</h4>
-                <AnchorLink href="#services" offset="90">
-                  <Button text="שירותים" />
-                </AnchorLink>
-                <AnchorLink href="#contact" offset="90">
-                  <Button classN="btn-c" text="צור קשר" />
-                </AnchorLink>
+                <animated.h1 style={animateProps}>אגרא יעוץ ותכנון בע"מ</animated.h1>
+                <animated.h4 style={animateProps}>ניהול ותכנון – מתארי, פרוגרמטי, פונקציונלי, הנדסי וסביבתי. עשרות שנות ניסיון ביעוץ ותכנון לתעשייה, לוגיסטיקה, משרדי ממשלה, רשויות ממלכתיות ומקומיות, פרויקטים סביבתיים.</animated.h4>
+                <animated.div style={animateProps} className="btns">
+                  <AnchorLink href="#services" offset="90">
+                    <Button text="שירותים" />
+                  </AnchorLink>
+                  <AnchorLink href="#contact" offset="90">
+                    <Button classN="btn-c" text="צור קשר" />
+                  </AnchorLink>
+                </animated.div>
               </div>
             </BgImg>
           </>
-        )
+        ))
       }}
     />
   )
