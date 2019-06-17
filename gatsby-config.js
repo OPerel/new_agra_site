b807c7a124206b4888b0247a77357a2934b2600e
Operel <oriperelman@gmail.com>
2019-06-17 14:39:19 +0300

    change site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `אתר אגרא החדש`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `יעוץ ותכנון. תכנון פונקציונלי, הכנת פרוגרמות, יעוץ סביבתי, וניהול פרוייקטים וצוותי תכנון.`,
     author: `@gatsbyjs`,
   },
   plugins: [
