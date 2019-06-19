8c200c333dbbeb17d61cadf1591047f95f5eef3c
Operel <oriperelman@gmail.com>
2019-06-19 16:59:24 +0300

    make side bar sticky with position fixed and js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Menus from './menus';
 import './sidebar.css';
 
 const sidebarMenus = ['שירותים', 'כתבות אחרונות'];
 
-const Sidebar = () => (
-  <div className="sidebar">
-    <Menus menuList={sidebarMenus}/>
-  </div>
-)
+class Sidebar extends Component {
+  sidebarPos = () => {
+    const side = document.getElementById('sidebar');
+    const stick = side.getBoundingClientRect().top;
+    window.pageYOffset > stick * 2.38
+    ? side.classList.add("sticky") 
+    : side.classList.remove("sticky");
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.sidebarPos)
+  }
+
+  render() {
+    return (
+      <div id="sidebar" className="sidebar">
+        <Menus menuList={sidebarMenus}/>
+      </div>
+    )
+  }
+}
 
 export default Sidebar;
