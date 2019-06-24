072dc316f60084e54b6ee64d5e54a10b262ca47c
Operel <oriperelman@gmail.com>
2019-06-24 17:54:17 +0300

    html code for "

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `אגרא יעוץ ותכנון בע"מ`,
+    title: `אגרא יעוץ ותכנון בע&quotמ`,
     description: `יעוץ ותכנון. תכנון פונקציונלי, הכנת פרוגרמות, יעוץ סביבתי, וניהול פרוייקטים וצוותי תכנון.`,
     author: `https://github.com/OPerel`,
   },
