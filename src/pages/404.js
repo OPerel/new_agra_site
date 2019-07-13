d30a84e8ea955dd325a596eea8d2e5a3777fb2e5
Operel <oriperelman@gmail.com>
2019-07-13 20:28:21 +0300

    add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React from "react"
-
+import { Link } from 'gatsby';
+import './404.css';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>אופס! 404</h1>
-    <p>העמוד שחיפשת לא קיים...</p>
+    <div className="nf-wrapper">
+      <div className="nf-img-bg"><div className="nf-img"><p>404</p></div></div>
+      <div className="not-found">
+        <h1>אופס!</h1>
+        <h4>העמוד שחיפשת לא קיים...</h4>
+        <Link to="/">חזרה לעמוד הראשי</Link>
+      </div>
+    </div>
   </Layout>
 )
 
